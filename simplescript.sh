962795b5df8603444d0538e69b8407f2790e7eb0
jmcc-uog <145256873+jmcc-uog@users.noreply.github.com>
2024-06-11 13:16:04 +0100

    Create simplescript.sh

---
+++ simplescript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create a file called success.txt and write "It was successful" into it
+echo "It was successful" > success.txt
+
+# Print a message indicating the script has run
+echo "File success.txt has been created and populated."
